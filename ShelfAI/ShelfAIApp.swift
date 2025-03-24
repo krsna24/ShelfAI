50481e97b0d58bcaf7af71ce54cd50a51f6e6821
Krsna Sharma <krushna2483@gmail.com>
2025-03-24 22:19:06 +0530

    Initial Commit

---
+++ ShelfAI/ShelfAIApp.swift
@@ -0,0 +1,20 @@
+//
+//  ShelfAIApp.swift
+//  ShelfAI
+//
+//  Created by Krsna  on 3/24/25.
+//
+
+import SwiftUI
+
+@main
+struct ShelfAIApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
